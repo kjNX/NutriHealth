569d18b15fd3e93a901ecb6d1d7dd1f1287b71a8
wisner <wisner.valdiviezo@unmsm.edu.pe>
2025-07-11 12:58:02 -0500

    TEMAS

--- app/src/main/java/com/unmsm/nutrihealth/ui/theme/Color.kt
+++ app/src/main/java/com/unmsm/nutrihealth/ui/theme/Color.kt
@@ -1,5 +1,6 @@
 package com.unmsm.nutrihealth.ui.theme
 
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -12,3 +13,13 @@ val Pink40 = Color(0xFF7D5260)
 val proteina = Color(0xFF79DC0F)
 val carbohidrato = Color(0xFF2196F3)
 val grasa =  Color(0xFFEE546B)
+
+val PastelColorScheme = lightColorScheme(
+    primary = Color(0xFF66BB6A), // Verde medio
+    onPrimary = Color.White,
+    secondary = Color(0xFFA8D8B9), // Verde pastel
+    secondaryContainer = Color(0xFFE3F2FD), // Azul pastel
+    background = Color(0xFFF8FFF8),
+    surface = Color(0xFFF1F8E9),
+    onSurface = Color(0xFF2E7D32)
+)
